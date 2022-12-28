2dd559cbe2b7209c7b8bb2f3ae42dbace7978612
Dmytro Makovey <dmakovey@yahoo.com>
2022-12-28 10:58:35 -0700

    Add proper checks for skipping/exclusion

--- main.go
+++ main.go
@@ -19,12 +19,46 @@ var (
 	moduleInfo     ModuleInfo
 )
 
+/*==================
+  Class: fileList
+*/
+type fileList []string
+
+func (e *fileList) String() string {
+	return fmt.Sprint(*e)
+}
+
+func (e *fileList) Set(value string) error {
+	for _, fn := range strings.Split(value, ",") {
+		*e = append(*e, fn)
+	}
+	return nil
+}
+
+func (e *fileList) Contains(value string) bool {
+	for _, fn := range *e {
+		if value == fn {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+  End of fileList
+  ===================*/
+
 type ModuleInfo struct {
-	Name          string
-	ExtensionsDir string
-	NwnDir        string
-	Installed     []string
-	Skipped       []string
+	Name              string   `json:"name"`
+	ExtensionsDir     string   `json:"extensionsDir"`
+	NwnDir            string   `json:"nwnDir"`
+	BackupExtension   string   `json:"backupExtension"`
+	OverwriteExisting bool     `json:"overwriteExisting"`
+	Files             fileList `json:"files"`
+	Installed         fileList `json:"installed"`
+	Skipped           fileList `json:"skipped"`
+	Excluded          fileList `json:"excluded"`
+	Saved             fileList `json:"saved"`
 }
 
 func walkDir(path string, d fs.DirEntry, err error) (e error) {
@@ -34,11 +68,15 @@ func walkDir(path string, d fs.DirEntry, err error) (e error) {
 	}
 	if !d.IsDir() {
 		shortPath := strings.Replace(path, moduleRootPath, "", 1)
-		moduleInfo.Installed = append(moduleInfo.Installed, string(shortPath[1:]))
+		moduleInfo.Files = append(moduleInfo.Files, string(shortPath[1:]))
 		// fmt.Println("..." + path)
 	}
 	return nil
 }
+
+/*=================
+  Copy functions
+*/
 
 type CopyFunc func(src, dst string) (int64, error)
 
@@ -81,14 +119,24 @@ func copyFile(src, dst string) (int64, error) {
 	return nBytes, err
 }
 
+/*
+  End of Copy Functions
+  =====================*/
+
 func main() {
 	var skipErrors bool
+	var dryRun bool
 	flag.StringVar(&moduleInfo.Name, "module", "", "")
 	flag.StringVar(&moduleInfo.ExtensionsDir, "extensions-dir", "Extensions", "")
 	flag.StringVar(&moduleInfo.NwnDir, "nwn-dir", ".", "")
 	flag.StringVar(&tgtDir, "target-dir", "", "")
+	flag.StringVar(&moduleInfo.BackupExtension, "backup-extension", ".bak", "")
 	flag.BoolVar(&skipErrors, "skip-errors", false, "Skip file errors")
+	flag.BoolVar(&moduleInfo.OverwriteExisting, "overwrite-existing", false, "Overwrite Existing files")
+	flag.BoolVar(&dryRun, "dry-run", false, "Dry Run")
+	flag.Var(&moduleInfo.Excluded, "excluded", "excluded files")
 	flag.Parse()
+
 	moduleRootPath, _ = filepath.Abs(filepath.Join(moduleInfo.NwnDir, moduleInfo.ExtensionsDir, moduleInfo.Name))
 	var basePath string
 	if tgtDir != "" {
@@ -101,28 +149,45 @@ func main() {
 	fmt.Println("Run pre-install checks...")
 	var checks bool
 	checks = true
-	for _, fpath := range moduleInfo.Installed {
+	var exists int
+
+	var copy CopyFunc
+
+	if dryRun {
+		copy = copyDry
+	} else {
+		copy = copyDry
+	}
+
+	for _, fpath := range moduleInfo.Files {
 		dst := filepath.Join(basePath, fpath)
+		exists = 0
+		excluded := moduleInfo.Excluded.Contains(fpath)
 		if _, err := os.Stat(dst); err == nil {
 			// path/to/whatever exists
 			fmt.Printf("Can't overwrite existing %s\n", dst)
 			checks = false
-			moduleInfo.Skipped = append(moduleInfo.Skipped, fpath)
+			exists = 1
 		} else if !errors.Is(err, os.ErrNotExist) {
 			// unsure whether file exist or does not
 			fmt.Printf("Uncertain existence of %s\n", dst)
 			checks = false
+			exists = -1
+		} else if !excluded {
+			// it's OK to install
+			moduleInfo.Installed = append(moduleInfo.Installed, fpath)
+		}
+		if exists != 0 && moduleInfo.OverwriteExisting && !excluded {
+			copy(dst, dst+moduleInfo.BackupExtension)
+			moduleInfo.Saved = append(moduleInfo.Saved, fpath+moduleInfo.BackupExtension)
+		} else if exists != 0 && !excluded {
 			moduleInfo.Skipped = append(moduleInfo.Skipped, fpath)
 		}
 	}
 
-	var copy CopyFunc
 	if !checks && !skipErrors {
 		fmt.Printf("Exiting due to previous errors\n")
 		os.Exit(1)
-		copy = copyDry
-	} else {
-		copy = copyDry
 	}
 
 	for _, fpath := range moduleInfo.Installed {
